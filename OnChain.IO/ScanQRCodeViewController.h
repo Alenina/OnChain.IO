d627933a1b37c65285cd4f5ce729d27f1e6f13f9
Cool <cool@Cools-Mac-2.local>
2015-01-26 07:39:10 -0800

    first commit

--- OnChain.IO/ScanQRCodeViewController.h
+++ OnChain.IO/ScanQRCodeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ScanQRCodeViewController.h
 //  OnChain.IO
 //
 //  Created by Cool on 1/26/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ScanQRCodeViewController : UIViewController
 
 @end
-
